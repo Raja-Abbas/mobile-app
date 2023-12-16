a4df33423d1d8babb67b1a78e277d617e7380a5c
abbas <abbasaffandi2002@gmail.com>
2023-12-16 12:08:12 -0800

    commit added

--- app/page.js
+++ app/page.js
@@ -1,21 +1,18 @@
 "use client";
-import React from 'react';
-import NoSSR from 'react-no-ssr';
-import Data from './Pages/data';
-import Navbar from './Pages/navbar';
-import { NextUIProvider } from "@nextui-org/system";
+import React from 'react'
+import Data from './Pages/data'
+import Navbar from './Pages/navbar'
+import {NextUIProvider} from "@nextui-org/system";
 
-function Page() {
+function page() {
   return (
-            <NoSSR>
-    <NextUIProvider>
-      <div>
-        <Navbar />
-          <Data />
-      </div>
-    </NextUIProvider>
-            </NoSSR>
-  );
+        <NextUIProvider>
+    <div>
+      <Navbar />
+      <Data />
+    </div>
+      </NextUIProvider>
+  )
 }
 
-export default Page;
+export default page

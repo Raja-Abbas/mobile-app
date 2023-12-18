1fde9eccfd6c06813a1e08250a1d6eca7c3994b2
abbas <abbasaffandi2002@gmail.com>
2023-12-18 04:52:28 -0800

    api integrated

--- app/Pages/card.jsx
+++ app/Pages/card.jsx
@@ -40,56 +40,98 @@ export default function App({ addToCartCallback }) {
 
   // Filter items with the selected category and limit to 14 items
   const filter = (product) =>
-    (selectedCategory.toLowerCase() === "all" ||
+    (selectedCategory.toLowerCase() === "all" 
+    ||
       product.category.toLowerCase() === selectedCategory.toLowerCase()) &&
     (product.category.toLowerCase() === "smartphones" ||
-      product.category.toLowerCase() === "laptops");
+      product.category.toLowerCase() === "laptops" ||
+      product.category.toLowerCase() === "fragrances" ||
+      product.category.toLowerCase() === "skincare"  ||
+      product.category.toLowerCase() === "groceries" ||
+       product.category.toLowerCase() === "home-decoration");
 
   const filteredProducts = products.filter(filter);
 
+
   return (
-    <div className='flex max-lg:flex-col lg:flex-row gap-6 z-10 w-[100%]'>
-      <div className="flex shadow-2xl border-r border-gray-300 bg-transparent max-lg:hidden flex-col flex-wrap pt-20 justify-start lg:px-5 xl:px-10 items-start gap-12 lg:w-[300px]">
+    <div className="flex max-lg:flex-col lg:flex-row gap-6 z-10 w-[100%]">
+      <div className="flex shadow-2xl border-r border-gray-300 bg-transparent max-lg:hidden flex-col flex-wrap pt-20 justify-start px-5 items-start gap-12 lg:w-[300px]">
         <div className="fixed">
-        <p className="text-3xl">Category:</p>
-        <div className="flex gap-6 flex-col mt-10">
-          <p
-            className={`px-3 py-1 bg-gray-300 hover:bg-gray-500 text-2xl rounded-lg cursor-pointer ${
-              selectedCategory === "all" && "bg-gray-500"
-            }`}
-            onClick={() => handleCategoryClick("all")}
-          >
-            All
-          </p>
-          <p
-            className={`px-3 py-1 bg-gray-300 hover:bg-gray-500 text-2xl rounded-lg cursor-pointer ${
-              selectedCategory === "laptops" && "bg-gray-500"
-            }`}
-            onClick={() => handleCategoryClick("laptops")}
-          >
-            Laptops
-          </p>
-          <p
-            className={`px-3 py-1 bg-gray-300 hover:bg-gray-500 text-2xl rounded-lg cursor-pointer ${
-              selectedCategory === "smartphones" && "bg-gray-500"
-            }`}
-            onClick={() => handleCategoryClick("smartphones")}
-          >
-            Smartphones
-          </p>
+          <p className="text-3xl">Category:</p>
+          <div className="flex gap-6 flex-col mt-10">
+            <p
+              className={`px-3 py-1 bg-gray-300 hover:bg-gray-500 text-2xl rounded-lg cursor-pointer transition-all duration-300 ${
+                selectedCategory === "all" && "bg-gray-500 text-white"
+              }`}
+              onClick={() => handleCategoryClick("all")}
+            >
+              All
+            </p>
+            <p
+              className={`px-3 py-1 bg-gray-300 hover:bg-gray-500 text-2xl rounded-lg cursor-pointer transition-all duration-300 ${
+                selectedCategory === "laptops" && "bg-gray-500 text-white"
+              }`}
+              onClick={() => handleCategoryClick("laptops")}
+            >
+              Laptops
+            </p>
+            <p
+              className={`px-3 py-1 bg-gray-300 hover:bg-gray-500 text-2xl rounded-lg cursor-pointer transition-all duration-300 ${
+                selectedCategory === "smartphones" && "bg-gray-500  text-white"
+              }`}
+              onClick={() => handleCategoryClick("smartphones")}
+            >
+              Smartphones
+            </p>
+            <p
+              className={`px-3 py-1 bg-gray-300 hover:bg-gray-500 text-2xl rounded-lg cursor-pointer transition-all duration-300 ${
+                selectedCategory === "fragrances" && "bg-gray-500 text-white"
+              }`}
+              onClick={() => handleCategoryClick("fragrances")}
+            >
+              Fragrances
+            </p>
+            <p
+              className={`px-3 py-1 bg-gray-300 hover:bg-gray-500 text-2xl rounded-lg cursor-pointer transition-all duration-300 ${
+                selectedCategory === "skincare" && "bg-gray-500 text-white"
+              }`}
+              onClick={() => handleCategoryClick("skincare")}
+            >
+              Skincare
+            </p>
+            <p
+              className={`px-3 py-1 bg-gray-300 hover:bg-gray-500 text-2xl rounded-lg cursor-pointer transition-all duration-300 ${
+                selectedCategory === "groceries" && "bg-gray-500 text-white"
+              }`}
+              onClick={() => handleCategoryClick("groceries")}
+            >
+              Groceries
+            </p>
+            <p
+              className={`px-3 py-1 bg-gray-300 hover:bg-gray-500 text-2xl rounded-lg cursor-pointer transition-all duration-300 ${
+                selectedCategory === "home-decoration" && "bg-gray-500 text-white"
+              }`}
+              onClick={() => handleCategoryClick("home-decoration")}
+            >
+              Home Decoration
+            </p>
+          </div>
         </div>
       </div>
-      </div>
-      <div className='flex flex-wrap mt-20 justify-center items-start gap-10 w-[100%]'>
+      <div className="flex flex-wrap mt-20 justify-center items-start gap-10 w-[100%]">
         {filteredProducts.map((product) => (
           <Card
             key={product.id}
-            className='max-lg:max-w-sm lg:max-w-xs max-h-[650px] hover:w-[100%] sm:flex-col hover:justify-center inline-flex hover:bg-gray-200 text-center hover:scale-[1.04] transition-all cursor-pointer ease-in-out items-start rounded-xl shadow-2xl bg-transparent'
+            className="max-lg:max-w-sm lg:max-w-xs max-h-[650px] hover:w-[100%] sm:flex-col hover:justify-center inline-flex hover:bg-gray-200 text-center hover:scale-[1.04] transition-all cursor-pointer ease-in-out items-start rounded-xl shadow-2xl bg-transparent"
           >
             <CardBody className="z-0 p-0 relative overflow-hidden flex justify-start items-start">
               <div className="flex justify-between w-full px-3 absolute top-5 z-50">
-              <p className="bg-gray-400 px-2 py-1 shadow-inner rounded-lg text-white">Discount: {product.discountPercentage}%</p>
-              <p className="bg-[#e35225] px-2 py-1 shadow-inner rounded-lg text-white">Rate: {product.rating}</p>
+                <p className="bg-gray-400 px-2 py-1 shadow-inner rounded-lg text-white">
+                  Discount: {product.discountPercentage}%
+                </p>
+                <p className="bg-[#e35225] px-2 py-1 shadow-inner rounded-lg text-white">
+                  Rate: {product.rating}
+                </p>
               </div>
               <Image
                 isBlurred
@@ -103,12 +145,12 @@ export default function App({ addToCartCallback }) {
                 {product.category}
               </p>
               <div className="flex w-full -mt-2 items-center justify-between">
-              <small className="text-lg px-2 rounded-lg text-center ">
-                {product.title}
-              </small>
-              <small className="text-xl inline-flex bg-sky-600 px-2 text-white rounded-lg text-center">
-                ${product.price}
-              </small>
+                <small className="text-lg px-2 rounded-lg text-center ">
+                  {product.title}
+                </small>
+                <small className="text-xl inline-flex bg-sky-600 px-2 text-white rounded-lg text-center">
+                  ${product.price}
+                </small>
               </div>
               <p className="text-xl px-2 rounded-lg text-center w-full">
                 Brand: <span className="">{product.brand}</span>

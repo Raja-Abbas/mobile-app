20f3afbdef8aba3cb958e2e8885030818701af86
abbas <abbasaffandi2002@gmail.com>
2023-12-18 10:18:27 -0800

    api integrated

--- app/Pages/StoreIcon/Main.jsx
+++ app/Pages/StoreIcon/Main.jsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from "react";
 import { Badge } from "@nextui-org/badge";
 import { CartIcon } from "./CartIcon";
-
+import Image from "next/image";
 export default function Main() {
   const [isSidebarVisible, setIsSidebarVisible] = useState(false);
   const [cartItems, setCartItems] = useState([]);
@@ -24,28 +24,39 @@ export default function Main() {
   const addToCart = (product) => {
     setCartItems([...cartItems, product]);
   };
+
   return (
     <div className="flex items-center gap-4 transition-all ease-in-out cursor-pointer text-white relative">
       <div className="flex items-center px-2 py-2" onClick={toggleSidebar}>
-        <Badge color="warning" content={cartItems.length} shape="circle" radius="lg">
+        <Badge
+          color="warning"
+          content={cartItems.length}
+          shape="circle"
+          radius="lg"
+        >
           <CartIcon size={100} />
         </Badge>
       </div>
 
       {isSidebarVisible && (
-        <div className="fixed pt-16 px-5 top-0 bottom-0 right-0 z-50 animate-bounces transition-all ease-in-out w-[30%] h-[100vh] bg-gray-300 shadow-2xl transform">
-          <button
-            className="absolute text-[40px] top-2 right-2 text-white cursor-pointer"
-            onClick={closeSidebar}
-          >
-            &#10006;
-          </button>
-
+        <div className="fixed pt-16 px-5 top-[100px] bottom-0 right-0 z-50 transition-all animate-drip-expand ease-in-out max-lg:w-[100%] lg:w-[25%] h-[100vh] bg-gray-300 shadow-2xl transform">
           <div>
             <p className="text-3xl">Sidebar Content</p>
             <ul>
-              {cartItems.map((item, index) => (
-                <li key={index}>{item.title}</li>
+              {cartItems.map((product, index) => (
+                <li key={index}>
+                  <div className="flex items-center z-50">
+                    <Image
+                      src={product.thumbnail}
+                      alt={product.title}
+                      className="h-8 w-8 object-cover mr-2"
+                    />
+                    <div>
+                      <p>{product.title}</p>
+                      <p>${product.price}</p>
+                    </div>
+                  </div>
+                </li>
               ))}
             </ul>
           </div>
--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 "use client";
-import 'tailwindcss/tailwind.css';
-import React from 'react'
-import Data from './Pages/data'
-import Navbar from './Pages/navbar'
+import "tailwindcss/tailwind.css";
+import React, { useState, useEffect } from "react";
+import Data from "./Pages/data";
+import Navbar from "./Pages/navbar";
 import { NextUIProvider } from "@nextui-org/system";
 
 // Add custom styles to your component
@@ -25,16 +25,75 @@ const styles = `
   }
 `;
 
-function page() {
+function Page() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const loadingTimer = setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+
+    return () => clearTimeout(loadingTimer);
+  }, []);
+
   return (
     <NextUIProvider>
       <style jsx>{styles}</style>
-      <div className='font-sans'>
-        <Navbar />
-        <Data />
-      </div>
+      {isLoading ? (
+        <div className="flex justify-center items-center h-screen">
+          <main>
+            <svg
+              class="ip"
+              viewBox="0 0 256 128"
+              width="256px"
+              height="128px"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <defs>
+                <linearGradient id="grad1" x1="0" y1="0" x2="1" y2="0">
+                  <stop offset="0%" stop-color="#5ebd3e" />
+                  <stop offset="33%" stop-color="#ffb900" />
+                  <stop offset="67%" stop-color="#f78200" />
+                  <stop offset="100%" stop-color="#e23838" />
+                </linearGradient>
+                <linearGradient id="grad2" x1="1" y1="0" x2="0" y2="0">
+                  <stop offset="0%" stop-color="#e23838" />
+                  <stop offset="33%" stop-color="#973999" />
+                  <stop offset="67%" stop-color="#009cdf" />
+                  <stop offset="100%" stop-color="#5ebd3e" />
+                </linearGradient>
+              </defs>
+              <g fill="none" stroke-linecap="round" stroke-width="16">
+                <g class="ip__track" stroke="#ddd">
+                  <path d="M8,64s0-56,60-56,60,112,120,112,60-56,60-56" />
+                  <path d="M248,64s0-56-60-56-60,112-120,112S8,64,8,64" />
+                </g>
+                <g stroke-dasharray="180 656">
+                  <path
+                    class="ip__worm1"
+                    stroke="url(#grad1)"
+                    stroke-dashoffset="0"
+                    d="M8,64s0-56,60-56,60,112,120,112,60-56,60-56"
+                  />
+                  <path
+                    class="ip__worm2"
+                    stroke="url(#grad2)"
+                    stroke-dashoffset="358"
+                    d="M248,64s0-56-60-56-60,112-120,112S8,64,8,64"
+                  />
+                </g>
+              </g>
+            </svg>
+          </main>
+        </div>
+      ) : (
+        <div className="font-sans">
+          <Navbar />
+          <Data />
+        </div>
+      )}
     </NextUIProvider>
   );
 }
 
-export default page;
+export default Page;

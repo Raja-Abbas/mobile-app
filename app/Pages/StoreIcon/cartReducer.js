2c225bc82b668f1df8338f008fb1a301b5fc3496
abbas <abbasaffandi2002@gmail.com>
2023-12-20 06:35:57 -0800

    redux added

--- app/Pages/StoreIcon/cartReducer.js
+++ app/Pages/StoreIcon/cartReducer.js
@@ -1,21 +1,32 @@
 // cartReducer.js
 import { ADD_TO_CART, REMOVE_FROM_CART } from "./cartActions";
 
-const initialState = {
-  cartItems: [],
+const getInitialCartState = () => {
+  const storedCartItems = localStorage.getItem("cartItems");
+  return {
+    cartItems: storedCartItems ? JSON.parse(storedCartItems) : [],
+  };
 };
+
+const initialState = getInitialCartState();
 
 const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TO_CART:
+      const updatedCartAdd = [...state.cartItems, action.payload];
+      localStorage.setItem("cartItems", JSON.stringify(updatedCartAdd));
       return {
         ...state,
-        cartItems: [...state.cartItems, action.payload],
+        cartItems: updatedCartAdd,
       };
     case REMOVE_FROM_CART:
+      const updatedCartRemove = state.cartItems.filter(
+        (item) => item.id !== action.payload
+      );
+      localStorage.setItem("cartItems", JSON.stringify(updatedCartRemove));
       return {
         ...state,
-        cartItems: state.cartItems.filter(item => item.id !== action.payload),
+        cartItems: updatedCartRemove,
       };
     default:
       return state;

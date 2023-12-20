1df99dac60caddcc6134a5d9d5b326deb6512caf
abbas <abbasaffandi2002@gmail.com>
2023-12-20 06:39:03 -0800

    redux added

--- app/Pages/StoreIcon/cartReducer.js
+++ app/Pages/StoreIcon/cartReducer.js
@@ -2,10 +2,16 @@
 import { ADD_TO_CART, REMOVE_FROM_CART } from "./cartActions";
 
 const getInitialCartState = () => {
-  const storedCartItems = localStorage.getItem("cartItems");
-  return {
-    cartItems: storedCartItems ? JSON.parse(storedCartItems) : [],
-  };
+  if (typeof window !== "undefined") {
+    const storedCartItems = localStorage.getItem("cartItems");
+    return {
+      cartItems: storedCartItems ? JSON.parse(storedCartItems) : [],
+    };
+  } else {
+    return {
+      cartItems: [],
+    };
+  }
 };
 
 const initialState = getInitialCartState();
@@ -13,21 +19,27 @@ const initialState = getInitialCartState();
 const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TO_CART:
-      const updatedCartAdd = [...state.cartItems, action.payload];
-      localStorage.setItem("cartItems", JSON.stringify(updatedCartAdd));
-      return {
-        ...state,
-        cartItems: updatedCartAdd,
-      };
+      if (typeof window !== "undefined") {
+        const updatedCartAdd = [...state.cartItems, action.payload];
+        localStorage.setItem("cartItems", JSON.stringify(updatedCartAdd));
+        return {
+          ...state,
+          cartItems: updatedCartAdd,
+        };
+      }
+      return state;
     case REMOVE_FROM_CART:
-      const updatedCartRemove = state.cartItems.filter(
-        (item) => item.id !== action.payload
-      );
-      localStorage.setItem("cartItems", JSON.stringify(updatedCartRemove));
-      return {
-        ...state,
-        cartItems: updatedCartRemove,
-      };
+      if (typeof window !== "undefined") {
+        const updatedCartRemove = state.cartItems.filter(
+          (item) => item.id !== action.payload
+        );
+        localStorage.setItem("cartItems", JSON.stringify(updatedCartRemove));
+        return {
+          ...state,
+          cartItems: updatedCartRemove,
+        };
+      }
+      return state;
     default:
       return state;
   }
